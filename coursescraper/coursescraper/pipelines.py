0713331e1d4fcd31e30cbf4b0281bc2c18929eee
Le Thanh Nguyen <u7594144@anu.edu.au>
2024-09-06 23:56:38 +1000

    optimized pipelines code

--- coursescraper/coursescraper/pipelines.py
+++ coursescraper/coursescraper/pipelines.py
@@ -9,6 +9,7 @@ from itemadapter import ItemAdapter
 import mysql.connector
 import os
 from dotenv import load_dotenv
+import logging
 
 load_dotenv()
 
@@ -16,14 +17,21 @@ class CoursescraperPipeline:
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
         
+        url = adapter.get('url')
+        # * Check if URL contains Error/Index
+        if "Error/Index" in url:
+            logging.warning(f"Filtered non-offered courses: {url}")
+            return None
+
         # * Strip leading and trailing whitespaces from strings
-        field_names = adapter.field_names()
-        for field_name in field_names:
-            if field_name == 'learning_outcomes':
-                learning_outcomes = adapter.get(field_name)
-                for i in range(len(learning_outcomes)):
-                    learning_outcomes[i] = learning_outcomes[i].strip()
-                adapter[field_name] = learning_outcomes
+        learning_outcomes = adapter.get("learning_outcomes")
+        for i in range(len(learning_outcomes)):
+            if "NA" in learning_outcomes[i]:
+                return None
+            learning_outcomes[i] = learning_outcomes[i].strip()
+        adapter['learning_outcomes'] = learning_outcomes
+
+        return item
         
 class SaveToMySQLPipeline:
     def __init__(self):
@@ -31,8 +39,32 @@ class SaveToMySQLPipeline:
             host = os.getenv('MYSQL_HOST'),
             user = os.getenv('MYSQL_USER'),
             password = os.getenv('MYSQL_PASSWORD'),
-            database = os.getenv('MYSQL_COURSESSDB')
+            database = os.getenv('MYSQL_DATABASE')
         )
 
         self.cur = self.conn.cursor()
+
+    def process_item(self, item , spider):
+        adapter = ItemAdapter(item)
+        course_code = adapter.get('code')
+        course_title = adapter.get('title')
+        course_url = adapter.get('url')
+        learning_outcomes = '\n'.join(adapter.get('learning_outcomes'))
+
+        self.cur.execute(
+            """
+                INSERT INTO courses (code, title, url, learning_outcomes)
+                VALUES (%s, %s, %s, %s)
+            """, (course_code, course_title, course_url, learning_outcomes)
+        )
+
+        self.conn.commit()
+
+    def close_spider(self, spider):
+
+        ## Close cursor & connection to database 
+        self.cur.close()
+        self.conn.close()
+
+
 
--- coursescraper/coursescraper/spiders/coursespider.py
+++ coursescraper/coursescraper/spiders/coursespider.py
@@ -1,5 +1,6 @@
 import scrapy
 from coursescraper.items import CourseItem
+import logging
 
 class CoursespiderSpider(scrapy.Spider):
     name = "coursespider"
@@ -9,21 +10,22 @@ class CoursespiderSpider(scrapy.Spider):
     def parse(self, response):
         courses = response.css("tbody tr")
         for course in courses:
-            course_url = course.css('td a').attrib['href'];
+            course_url = course.css('td a').attrib['href']
             yield scrapy.Request(course_url, callback=self.parse_course_page)
 
 
     # * Parse each course
     def parse_course_page(self, response):
+        # if response.status == 404:
+        #     logging.warning(f"Filtered non-offered courses: {response.url}")
+        #     return
         course_item = CourseItem()
         course_item['url'] = response.url
         course_item['title'] = response.css('span.intro__degree-title__component ::text').get()
         course_item['code'] = response.css('span.molecule__label a ::text').get()
         course_item['learning_outcomes'] = response.css('h2#learning-outcomes + p + ol li ::text').getall() # stored as an array
 
-        # TODO: course_item['prerequisites'] (hardest task)
-        
-
+        # TODO: course_item['prerequisites']
         yield course_item
         
 
